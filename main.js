a8c451bb3e3fa5d7edf46525f4a397ac86e9cfde
Josh Allan <allan.josh07@gmail.com>
2020-09-16 16:08:20 -0400

    Updated the sizing of the h1 and the table columns

--- main.js
+++ main.js
@@ -2,7 +2,18 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 $("#localTime").text(moment().format("h:mm a"));
 
+
+// Change class of past hours to the "past" class
+
+
+// Change class of current hour to the "current" class
+
+
+// Change class of upcoming hours to the "upcoming" class
+
+
+
 // Save user input when the save button is clicked
 $(".fa-save").on("click", function(){
-    console.log("This doesn't save the input yet.")
+    console.log("This doesn't save the input yet.") // placeholder -- not ready yet.
 });
