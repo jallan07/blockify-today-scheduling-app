cbc12535b693399b9720288bf8154cb263ae0382
Josh Allan <allan.josh07@gmail.com>
2020-09-16 15:48:47 -0400

    Updated the textarea to be slightly larger and removed some of the templated class styling that was in the files to begin with.

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 // Display current date and time in the jumbotron
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 $("#localTime").text(moment().format("h:mm a"));
+
+// Save user input when the save button is clicked
+$(".fa-save").on("click", function(){
+    console.log("This doesn't save the input yet.")
+});
