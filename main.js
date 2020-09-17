819db7b472d1fc36b6a537530eca2604dc83c735
Josh Allan <allan.josh07@gmail.com>
2020-09-17 14:58:44 -0400

    Added logic to the save button function that will set items into local storage and retrieve from local storage on load. Still need to add code that will save the textarea message to local storage. But it currently saves the hour block to storage.

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 // Document ready?
 $(document).ready(function() {
 
-// Call the retrieve local storage function upon page load
-getLocalStorage();
+// Retrieve local storage function upon page load
+var storedItems_9am = JSON.parse(localStorage.getItem("storedItems_9am")) || [];
+var storedItems_10am = JSON.parse(localStorage.getItem("storedItems_10am")) || [];
+var storedItems_11am = JSON.parse(localStorage.getItem("storedItems_11am")) || [];
+var storedItems_12pm = JSON.parse(localStorage.getItem("storedItems_12pm")) || [];
+var storedItems_1pm = JSON.parse(localStorage.getItem("storedItems_1pm")) || [];
+var storedItems_2pm = JSON.parse(localStorage.getItem("storedItems_2pm")) || [];
+var storedItems_3pm = JSON.parse(localStorage.getItem("storedItems_3pm")) || [];
+var storedItems_4pm = JSON.parse(localStorage.getItem("storedItems_4pm")) || [];
+var storedItems_5pm = JSON.parse(localStorage.getItem("storedItems_5pm")) || [];
+
+
 // Call the update row classes function upon page load
 updateRowClasses();
 // ——————————————————————— //
@@ -21,9 +31,60 @@ update();
 setInterval(update, 1000);
 
 // Set local storage -- Save user input when the save button is clicked
-$(".fa-save").on("click", function(){
+$(".fa-save").on("click", function(e){
     // Save textarea input to local storage
-    
+        // Prevent previous entries from being cleared automatically
+        e.preventDefault();
+        // Set the new data items
+        var newData = {
+            hour: $(this).data("toast"), // 
+            message: "" // How do I target the specific textarea? Currently, each one has an ID of "__am/pmMessage"
+        };
+        console.log(newData);
+        
+        // Push the newData to the corresponding stored arrays and set to local storage
+        if ($(this).data("toast") === "9:00 am") {
+            storedItems_9am.push(newData);
+            localStorage.setItem("storedItems_9am", JSON.stringify(storedItems_9am));
+        }
+        if ($(this).data("toast") === "10:00 am") {
+            storedItems_10am.push(newData);
+            localStorage.setItem("storedItems_10am", JSON.stringify(storedItems_10am));
+        }
+        if ($(this).data("toast") === "11:00 am") {
+            storedItems_11am.push(newData);
+            localStorage.setItem("storedItems_11am", JSON.stringify(storedItems_11am));
+        }
+        if ($(this).data("toast") === "12:00 pm") {
+            storedItems_12pm.push(newData);
+            localStorage.setItem("storedItems_12pm", JSON.stringify(storedItems_12pm));
+        }
+        if ($(this).data("toast") === "1:00 pm") {
+            storedItems_1pm.push(newData);
+            localStorage.setItem("storedItems_1pm", JSON.stringify(storedItems_1pm));
+        }
+        if ($(this).data("toast") === "2:00 pm") {
+            storedItems_2pm.push(newData);
+            localStorage.setItem("storedItems_2pm", JSON.stringify(storedItems_2pm));
+        }
+        if ($(this).data("toast") === "3:00 pm") {
+            storedItems_3pm.push(newData);
+            localStorage.setItem("storedItems_3pm", JSON.stringify(storedItems_3pm));
+        }
+        if ($(this).data("toast") === "4:00 pm") {
+            storedItems_4pm.push(newData);
+            localStorage.setItem("storedItems_4pm", JSON.stringify(storedItems_4pm));
+        }
+        if ($(this).data("toast") === "5:00 pm") {
+            storedItems_5pm.push(newData);
+            localStorage.setItem("storedItems_5pm", JSON.stringify(storedItems_5pm));
+        }
+
+
+
+
+
+
     // Toast notification
         // Establish the toast div as the variable x so we can manipulate it more easily
         var x = $(".toast");
@@ -33,7 +94,7 @@ $(".fa-save").on("click", function(){
         var inspo = phraseArr[Math.floor(Math.random() * phraseArr.length)];
         // Set the text for the toast notification
         $(".toast-body").text(inspo);
-        $("small").text($(this).data("toast"));
+        $("small").text("Items scheduled for " + $(this).data("toast"));
         // After 3 seconds, remove the notification
         setTimeout(function(){
             x.addClass("d-none"); }
@@ -64,12 +125,6 @@ $('.table-secondary').each(row => {
 
 
 // Change class of upcoming hours to the "upcoming" class
-
-
-// Retrieve local storage
-function getLocalStorage(){
-    console.log("Local storage has been retrieved.")
-};
            
 
 // ——————————————————————— //
